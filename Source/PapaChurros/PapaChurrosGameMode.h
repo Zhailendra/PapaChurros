984a038ded3809b55c675bc74c7ea13ff62d200e
Zhailendra <shailendrablondel@outlook.fr>
2025-03-13 22:50:51 +0100

    [+] Project Init with Android settings

---
+++ Source/PapaChurros/PapaChurrosGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PapaChurrosGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APapaChurrosGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APapaChurrosGameMode();
+};
+
+
+
