34674cc41507dc962357c04493965ddaa0692162
Zhailendra <shailendrablondel@outlook.fr>
2025-03-14 03:02:47 +0100

    [+] Player base BP & Object pool plugins

--- Source/PapaChurros/PapaChurros.Build.cs
+++ Source/PapaChurros/PapaChurros.Build.cs
@@ -8,7 +8,7 @@ public class PapaChurros : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ObjectPooling" });
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 	}
 }
